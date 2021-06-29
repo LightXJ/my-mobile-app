c9316749adee394ebe22e59268833487f3382a87
LightXJ <wxjneu@gmail.com>
2021-06-29 15:38:44 +0800

    不兼容iframe

--- src/App.js
+++ src/App.js
@@ -1,43 +1,25 @@
-import { Pagination, Icon } from 'antd-mobile';
-import { Button } from 'zarm';
-import './App.scss';
-
-const locale = {
-  prevText: 'Prev',
-  nextText: 'Next',
-};
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './Home';
 
 
 const App = () => (
-  <div className="pagination-container home-page" >
-    <div className="box">我是盒子</div>
-    <p className="describe">我是说明我是说明我是说明我是说明我是说明我是说明我是说明我是说明</p>
+  <Router>
+    <div>
 
-    <div className="title">以下是ant-mobile组件</div>
-    <Pagination total={5} current={1} locale={locale} />
-
-    <Pagination total={5}
-      className="custom-pagination-with-icon"
-      current={1}
-      locale={{
-        prevText: (<span className="arrow-align"><Icon type="left" />上一步</span>),
-        nextText: (<span className="arrow-align">下一步<Icon type="right" /></span>),
-      }}
-    />
-
-
-    <Pagination simple total={5} current={1} locale={locale} />
-
-
-    <Pagination mode="number" total={5} current={3} />
-
-
-    <Pagination mode="pointer" total={5} current={2} style={{ marginBottom: '16px' }} />
-
-    {/* <div className="title">以下是Zarm组件</div>
-    <Button>default</Button>
-    <Button theme="primary">primary</Button> */}
-  </div>
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
 );
 
 
---
+++ src/Home.js
@@ -0,0 +1,43 @@
+import { Pagination, Icon } from 'antd-mobile';
+import { Button } from 'zarm';
+import './Home.scss';
+
+const locale = {
+  prevText: 'Prev',
+  nextText: 'Next',
+};
+
+
+const Home = () => (
+  <div className="pagination-container home-page" >
+    <div className="box">我是盒子</div>
+    <p className="describe">我是说明我是说明我是说明我是说明我是说明我是说明我是说明我是说明</p>
+
+    <div className="title">以下是ant-mobile组件</div>
+    <Pagination total={5} current={1} locale={locale} />
+
+    <Pagination total={5}
+      className="custom-pagination-with-icon"
+      current={1}
+      locale={{
+        prevText: (<span className="arrow-align"><Icon type="left" />上一步</span>),
+        nextText: (<span className="arrow-align">下一步<Icon type="right" /></span>),
+      }}
+    />
+
+
+    <Pagination simple total={5} current={1} locale={locale} />
+
+
+    <Pagination mode="number" total={5} current={3} />
+
+
+    <Pagination mode="pointer" total={5} current={2} style={{ marginBottom: '16px' }} />
+
+    {/* <div className="title">以下是Zarm组件</div>
+    <Button>default</Button>
+    <Button theme="primary">primary</Button> */}
+  </div>
+);
+
+export default Home;
